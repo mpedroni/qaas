3766cb8bd7f6484b4098d09991fffaad6cf4347e
Matheus Pedroni <mpedroni77@gmail.com>
2021-08-03 22:46:13 -0300

    first commit

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    root: true,
+    env: {
+      node: true,
+    },
+    extends: ['plugin:vue/essential', '@vue/airbnb', 'plugin:prettier/recommended'],
+    parserOptions: {
+      parser: 'babel-eslint',
+    },
+    plugins: ['prettier'],
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'import/extensions': 'off',
+      'no-shadow': 'off',
+    },
+  };
