33649ad37eec1d259a7ef32ea0696965d9128821
Matheus Pedroni <mpedroni77@gmail.com>
2021-08-05 21:43:07 -0300

    create default button

---
+++ frontend/src/components/QBtn.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-btn v-bind="$attrs" v-on="$listeners" outlined>
+    <v-icon v-if="icon" left v-text="icon" />{{ label }}
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'QBtn',
+
+  props: {
+    icon: {
+      type: String,
+      default: '',
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
