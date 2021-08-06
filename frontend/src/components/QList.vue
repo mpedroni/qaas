efe7f4c61176dd628669de9cefe46068cf5fa61d
Matheus Pedroni <mpedroni77@gmail.com>
2021-08-05 23:49:24 -0300

    create default list component

---
+++ frontend/src/components/QList.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-list v-bind="$attrs" v-on="$listeners" outlined rounded class="px-0">
+    <slot />
+  </v-list>
+</template>
+
+<script>
+export default {
+  name: 'QList',
+};
+</script>
