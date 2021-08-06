2bd3f9ac588fb6def3de0f6c15e387625aa43820
Matheus Pedroni <mpedroni77@gmail.com>
2021-08-05 23:49:56 -0300

    apply font-weight-bold in the card's title

--- frontend/src/components/QCard.vue
+++ frontend/src/components/QCard.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <v-toolbar v-if="title" flat dense color="transparent">
-      <v-toolbar-title class="grey--text text--darken-3 text-h4">{{ title }}</v-toolbar-title>
+      <v-toolbar-title class="grey--text text--darken-3 text-h4 font-weight-bold">
+        {{ title }}
+      </v-toolbar-title>
     </v-toolbar>
     <v-card v-bind="$attrs" v-on="$listeners" rounded>
       <slot />
