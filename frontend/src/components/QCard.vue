0e23ef615fcb1abb25cfba03626fb4874f30b3bc
Matheus Pedroni <mpedroni77@gmail.com>
2021-08-05 21:43:17 -0300

    create default card

---
+++ frontend/src/components/QCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-toolbar v-if="title" flat dense color="transparent">
+      <v-toolbar-title class="grey--text text--darken-3 text-h4">{{ title }}</v-toolbar-title>
+    </v-toolbar>
+    <v-card v-bind="$attrs" v-on="$listeners" rounded>
+      <slot />
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QCard',
+
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
