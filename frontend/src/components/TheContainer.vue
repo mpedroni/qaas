b916e0544cb9d6c903df6643f6eea24ea9141bac
Matheus Pedroni <mpedroni77@gmail.com>
2021-08-05 21:44:19 -0300

    create default view's container

---
+++ frontend/src/components/TheContainer.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container id="container">
+    <slot />
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'TheContainer',
+};
+</script>
+
+<style scoped>
+#container {
+  max-width: 70vw;
+}
+</style>
