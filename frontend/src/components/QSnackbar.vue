2511373db09e31323c34e3af0cd8fa19285f60b1
Matheus Pedroni <mpedroni77@gmail.com>
2021-08-08 20:39:26 -0300

    create default Snackbar component

---
+++ frontend/src/components/QSnackbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-snackbar v-model="snackbar" right top rounded :color="color" :timeout="timeout">
+    {{ message }}
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  name: 'QSnackbar',
+
+  data: () => ({
+    snackbar: false,
+    message: '',
+    color: '',
+    timeout: null,
+  }),
+
+  methods: {
+    show(message, color, timeout = 5000) {
+      this.message = message;
+      this.color = color;
+      this.timeout = timeout;
+
+      this.snackbar = true;
+    },
+  },
+};
+</script>
