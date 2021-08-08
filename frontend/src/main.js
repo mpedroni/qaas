05bab4c4b218d5c18ef4b9fe142297b62d2b676a
Matheus Pedroni <mpedroni77@gmail.com>
2021-08-08 20:38:59 -0300

    include axios (client http) dep

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './config/vuetify';
 import router from './router';
+import api from './config/api';
 
 Vue.config.productionTip = false;
+
+Vue.prototype.$api = api;
 
 new Vue({
   vuetify,
